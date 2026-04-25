8ba9001f57bc0f2f3f47c4a5115efb5f632dd3ee
leo <leo250105but@gmail.com>
2026-04-25 20:10:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // !!! COLA TUAS CHAVES DO FIREBASE AQUI !!!
